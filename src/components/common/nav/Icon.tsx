7ca91b6f25c35516bf0cbfa92fc442b8db5000e8
reasat-rafio <rafioar96@gmail.com>
2021-09-04 16:33:13 +0600

    ⚡️ navbar user avatar added

--- src/components/common/nav/Icon.tsx
+++ src/components/common/nav/Icon.tsx
@@ -1,12 +1,15 @@
 import clsx from 'clsx'
 import { AnimatePresence, motion } from 'framer-motion'
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { Fragment, useState } from 'react'
 import { searchbarVariants } from '../../../../libs/animation'
 import { useWindowSize } from '../../../../libs/hooks'
 import { SHOW_CART_SIDE_MENU, SHOW_SEARCH_PAGE, SHOW_SIDE_MENU } from '../../../store/dom'
 import { useAppDispatch, useAppSelector } from '../../../store/hooks'
 import { selectProduct } from '../../../store/product'
+import { Menu, Transition } from '@headlessui/react'
+import { useUser } from '@auth0/nextjs-auth0'
+import Link from 'next/link'
 
 interface IIcons {
     title: string
@@ -22,6 +25,8 @@ export const Icon: React.FC<{
     const { inCartProducts } = useAppSelector(selectProduct)
     const [showSearchBar, setShowSearchBar] = useState<boolean>(false)
 
+    const { user, isLoading } = useUser()
+
     const _icons: IIcons[] = [
         {
             title: 'search',
@@ -33,11 +38,7 @@ export const Icon: React.FC<{
             url: '/static/svg/Cart.svg',
             uniqueClasses: ' flex-center',
         },
-        {
-            title: 'person',
-            url: '/static/svg/Person.svg',
-            uniqueClasses: 'hidden lg:flex',
-        },
+
         {
             title: 'menu',
             url: '/static/svg/Menu.svg',
@@ -65,12 +66,13 @@ export const Icon: React.FC<{
                     </>
                 )}
             </AnimatePresence>
-            <div className="flex justify-center items-center space-x-3 relative">
+            <div className="flex space-x-3 relative justify items-center">
                 {_icons.map(({ title, url, uniqueClasses }, index) => (
                     <div
                         key={index}
                         className={clsx(
-                            `hover:scale-105 transition-all duration-100 my-auto `,
+                            title === 'cart' && 'relative',
+                            `transition-all duration-100 my-auto  flex items-center justify-center`,
                             uniqueClasses && uniqueClasses,
                         )}
                         onClick={() => {
@@ -82,21 +84,88 @@ export const Icon: React.FC<{
                                 setShowSearchBar((prev) => !prev)
                         }}
                     >
-                        <div className={clsx(title === 'cart' && 'relative')}>
-                            {title === 'cart' && inCartProducts.length > 0 && (
-                                <span className="animate-bounce absolute inline-flex top-0 right-0 h-2 w-2 z-10 rounded-full bg-red-600"></span>
-                            )}
-                            <Image
-                                className="nav-icon nav-icon-styles "
-                                src={url}
-                                layout="intrinsic"
-                                height={windowWidth >= 1024 ? '30' : '25'}
-                                width={windowWidth >= 1024 ? '30' : '25'}
-                                alt={title}
-                            />
-                        </div>
+                        {title === 'cart' && inCartProducts.length > 0 && (
+                            <span className="animate-bounce absolute inline-flex top-0 right-0 h-2 w-2 z-10 rounded-full bg-red-600"></span>
+                        )}
+                        <Image
+                            className="nav-icon nav-icon-styles my-auto"
+                            src={url}
+                            layout="intrinsic"
+                            height={windowWidth >= 1024 ? '30' : '25'}
+                            width={windowWidth >= 1024 ? '30' : '25'}
+                            alt={title}
+                        />
                     </div>
                 ))}
+
+                <Menu as="div" className="mx-3 relative hidden lg:block">
+                    <div>
+                        <Menu.Button className="bg-yellow flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-yellow-300">
+                            <span className="sr-only">Open user menu</span>
+
+                            {!user && !isLoading && (
+                                <img
+                                    className=" h-[30px] w-[30px] p-1 rounded-full "
+                                    src="/static/svg/Person.svg"
+                                    alt=""
+                                />
+                            )}
+
+                            {user && !isLoading && (
+                                <img
+                                    className=" h-[30px] w-[30px] rounded-full"
+                                    src={user.picture as string}
+                                    alt=""
+                                />
+                            )}
+                        </Menu.Button>
+                    </div>
+                    <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                    >
+                        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            {!user && !isLoading && (
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <Link href="/api/auth/login">
+                                            <a
+                                                className={clsx(
+                                                    active ? 'bg-gray-100' : '',
+                                                    'block px-4 py-2 text-sm text-gray-700',
+                                                )}
+                                            >
+                                                Login / Register
+                                            </a>
+                                        </Link>
+                                    )}
+                                </Menu.Item>
+                            )}
+
+                            {user && !isLoading && (
+                                <Menu.Item>
+                                    {({ active }) => (
+                                        <Link href="/api/auth/logout">
+                                            <a
+                                                className={clsx(
+                                                    active ? 'bg-gray-100' : '',
+                                                    'block px-4 py-2 text-sm text-gray-700',
+                                                )}
+                                            >
+                                                Sign out
+                                            </a>
+                                        </Link>
+                                    )}
+                                </Menu.Item>
+                            )}
+                        </Menu.Items>
+                    </Transition>
+                </Menu>
             </div>
         </>
     )
